b79bc7e2ca5458d41d2308b377233211fbaf53e7
Adriano <adriano.selaviles@gmail.com>
2020-01-09 14:07:48 -0800

    simpler checksum

--- sum.go
+++ sum.go
@@ -1,13 +1,9 @@
 package rdtp
 
 func computeChecksum(p *Packet) uint16 {
-	csum, length := uint16(0), len(p.Payload)-1
-	for i := 0; i < length; i += 2 {
-		csum += uint16(p.Payload[i]) << 8
-		csum += uint16(p.Payload[i+1])
+	csum := uint16(0)
+	for i := 0; i < len(p.Payload); i++ {
+		csum += uint16(p.Payload[i])
 	}
-	if len(p.Payload)%2 == 1 {
-		csum += uint16(p.Payload[length]) << 8
-	}
-	return ^uint16(csum)
+	return uint16(csum)
 }
