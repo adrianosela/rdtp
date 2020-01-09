2d629f0f5226271c74087d6a2176da5a9b7e2f0a
Adriano <adriano.selaviles@gmail.com>
2020-01-09 15:05:55 -0800

    adjustment of ipv4 hader size

--- lim.go
+++ lim.go
@@ -2,7 +2,7 @@ package rdtp
 
 const (
 	// MaxPacketBytes is the maximum size of an RDTP packet incl. header
-	MaxPacketBytes = 65536
+	MaxPacketBytes = 65516 // 65536 - IPv4 Header (20 bytes)
 	// MinHeaderBytes is the byte size of an RDTP header
 	MinHeaderBytes = 8
 )
