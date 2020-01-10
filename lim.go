37463ac81ddf51a1051900781accd04b7b464847
Adriano Sela Aviles <adriano.selaviles@gmail.com>
2020-01-09 18:25:53 -0800

    Update lim.go

--- lim.go
+++ lim.go
@@ -2,7 +2,7 @@ package rdtp
 
 const (
 	// MaxPacketBytes is the maximum size of an RDTP packet incl. header
-	MaxPacketBytes = 65516 // 65536 - IPv4 Header (20 bytes)
+	MaxPacketBytes = 65515 // 65535 - IPv4 Header (20 bytes)
 	// MinHeaderBytes is the byte size of an RDTP header
 	MinHeaderBytes = 8
 )
