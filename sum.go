0c6fda82a1c155a41c181e79264ac53deef5e39d
Adriano <adriano.selaviles@gmail.com>
2020-01-17 15:14:07 -0800

    checksum impl'd

--- sum.go
+++ sum.go
@@ -1,10 +1,16 @@
-package rdtp
+package main
 
-// ones complement sum of payload bytes
 func computeChecksum(p *Packet) uint16 {
 	csum := uint16(0)
+	csum += p.SrcPort
+	csum += p.DstPort
+	csum += p.Length
 	for i := 0; i < len(p.Payload); i++ {
 		csum += uint16(p.Payload[i])
 	}
-	return ^uint16(csum)
+	return ^csum
+}
+
+func verifyChecksum(p *Packet) bool {
+	return p.Checksum == computeChecksum(p)
 }
