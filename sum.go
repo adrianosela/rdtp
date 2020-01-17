248d55945785774d03f8ccc159e6214de4e024b6
Adriano Sela Aviles <adriano.selaviles@gmail.com>
2020-01-17 14:41:50 -0800

    Update sum.go

--- sum.go
+++ sum.go
@@ -1,10 +1,10 @@
 package rdtp
 
-// this checksum can be improved
+// ones complement sum of payload bytes
 func computeChecksum(p *Packet) uint16 {
 	csum := uint16(0)
 	for i := 0; i < len(p.Payload); i++ {
 		csum += uint16(p.Payload[i])
 	}
-	return uint16(csum)
+	return ^uint16(csum)
 }
