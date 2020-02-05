1835025c42765820ff41f6fadc7cf8032469534a
Adriano <adriano.selaviles@gmail.com>
2020-02-05 12:21:11 -0800

    add proto number

--- packet.go
+++ packet.go
@@ -12,6 +12,11 @@ const (
 	// MaxPayloadByteSize is the maximum size of a payload that a single RDTP
 	// packet can carry
 	MaxPayloadByteSize = MaxPacketBytes - HeaderByteSize
+
+	// IPPROTO_RDTP is the protocol number for RDTP packets over IP
+	// The value 157 (0x9D) is unassigned as per:
+	// https://en.wikipedia.org/wiki/List_of_IP_protocol_numbers
+	IPPROTO_RDTP = 0x9D
 )
 
 // Packet is an RDTP packet
