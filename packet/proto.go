8765b46c2bae48bd43a211237dc5b2b6e2de3d5f
Adriano Sela Aviles <adriano.selaviles@gmail.com>
2020-02-07 20:10:51 -0800

    Update proto.go

--- packet/proto.go
+++ packet/proto.go
@@ -2,7 +2,7 @@ package packet
 
 const (
 	// MaxPacketBytes is the maximum size of an RDTP packet incl. header
-	MaxPacketBytes = 65515 // 65535 - IPv4 Header (20 bytes)
+	MaxPacketBytes = 1500 // will chunk otherwise
 	// HeaderByteSize is the byte size of an RDTP header
 	HeaderByteSize = 9
 	// MaxPayloadByteSize is the maximum size of a payload that a single RDTP
