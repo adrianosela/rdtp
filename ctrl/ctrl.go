5cdc9420d204fb067270def0b94a2a44c42ee7b5
Adriano <adriano.selaviles@gmail.com>
2020-04-04 00:01:05 -0700

    added ctrl

--- ctrl/ctrl.go
+++ ctrl/ctrl.go
@@ -3,15 +3,22 @@ package ctrl
 import (
 	"sync"
 
+	"github.com/adrianosela/rdtp/netwk"
 	"github.com/adrianosela/rdtp/socket"
+	"github.com/pkg/errors"
 )
 
-// Controller represents the rdtp controller. It essentially
-// foresees the following movement of data:
+// Controller represents the rdtp controller.
+//
+// It essentially foresees the following movement of data:
 // - outbound: user =msg=> socket =pck=> atc =pck=> netwk
 // - inbound: nwtwk =pck=> atc =pck=> socket =msg=> user
 type Controller struct {
 	sync.RWMutex
+
+	// network is an interface that takes packets
+	// and ships them out to the network
+	network *netwk.Network
 
 	// sockets is a map of sockets where each socket's
 	// unique identifier is "laddr:lport raddr:rport",
@@ -20,8 +27,50 @@ type Controller struct {
 }
 
 // NewController returns an initialized rdtp controller
-func NewController() *Controller {
-	return &Controller{
-		sockets: make(map[string]*socket.Socket),
+func NewController() (*Controller, error) {
+	nw, err := netwk.NewNetwork()
+	if err != nil {
+		return nil, errors.Wrap(err, "could not attach controller to network")
 	}
+	return &Controller{
+		network: nw,
+		sockets: make(map[string]*socket.Socket),
+	}, nil
+}
+
+// Get gets a socket given its id
+func (ctrl *Controller) Get(id string) (*socket.Socket, error) {
+	ctrl.RLock()
+	defer ctrl.RUnlock()
+
+	s, ok := ctrl.sockets[id]
+	if !ok {
+		return nil, errors.New("socket address not active")
+	}
+	return s, nil
+}
+
+// Put attaches a socket to the controller
+func (ctrl *Controller) Put(s *socket.Socket) error {
+	ctrl.Lock()
+	defer ctrl.Unlock()
+
+	id := s.ID()
+	if _, ok := ctrl.sockets[id]; ok {
+		return errors.New("socket address already in use")
+	}
+	ctrl.sockets[id] = s
+	return nil
+}
+
+// Evict removes a socket given its id
+func (ctrl *Controller) Evict(id string) error {
+	ctrl.Lock()
+	defer ctrl.Unlock()
+
+	if _, ok := ctrl.sockets[id]; !ok {
+		return errors.New("socket address not active")
+	}
+	delete(ctrl.sockets, id)
+	return nil
 }
---
+++ ctrl/mux.go
@@ -0,0 +1,11 @@
+package ctrl
+
+import (
+	"github.com/adrianosela/rdtp/packet"
+)
+
+// DeliverPacket delivers an inbound packet to its destination
+func (ctrl *Controller) DeliverPacket(p *packet.Packet) error {
+	// TODO
+	return nil
+}
